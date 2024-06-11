b5ae927e8ab867654c1e5025d956932416152b85
You can call me PIPOT <139671586+Afififaiz@users.noreply.github.com>
2024-06-12 06:39:54 +0800

    Update 4_📊 Dataset.py

--- pages/4_📊 Dataset.py
+++ pages/4_📊 Dataset.py
@@ -30,8 +30,25 @@ def main():
     # Create a Sankey chart to visualize relationships between selected attributes
     st.write("### Features Distribution Between Gender")
 
+     # Map numerical values to labels
+    gender_map = {0: 'Male', 1: 'Female'}
+    cp_map = {0: 'Typical Angina', 1: 'Atypical Angina', 2: 'Non-Anginal Pain', 3: 'Asymptomatic'}
+    fbs_map = {0: 'Below 120', 1: 'Above 120'}
+    restecg_map = {0: 'Normal', 1: 'ST-T Wave Abnormality', 2: 'Left Ventricular Hypertrophy'}
+    exang_map = {0: 'No', 1: 'Yes'}
+    slope_map = {0: 'Upsloping', 1: 'Flat', 2: 'Downsloping'}
+    thal_map = {3: 'Normal', 6: 'Fixed Defect', 7: 'Reversible Defect'}
+
+    data['Gender'] = data['sex'].map(gender_map)
+    data['cp'] = data['cp'].map(cp_map)
+    data['fbs'] = data['fbs'].map(fbs_map)
+    data['restecg'] = data['restecg'].map(restecg_map)
+    data['exang'] = data['exang'].map(exang_map)
+    data['slope'] = data['slope'].map(slope_map)
+    data['thal'] = data['thal'].map(thal_map)
+
     # Filter out 'Gender' from the list of columns
-    attribute_options = [col for col in data.columns if col != 'Gender']
+    attribute_options = [col for col in data.columns if col != 'Gender' and data[col].dtype == 'object']
 
     # Select attribute for Sankey chart
     selected_attribute = st.selectbox("Select an attribute:", attribute_options)
@@ -52,6 +69,7 @@ def main():
     )])
 
     st.plotly_chart(fig)
+
 
     # Create scatter plots to visualize relationships between numerical features
     st.write("### Serum Cholestrol Between Gender")
@@ -86,9 +104,10 @@ def main():
     st.write("### Correlation Heatmap")
     numeric_data = data.select_dtypes(include=['float64', 'int64'])  # Select only numeric columns
     corr = numeric_data.corr()
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(15, 8))
     heatmap = sns.heatmap(corr, annot=True, cmap='coolwarm', center=0)
     st.pyplot(plt)
+    
 
 # Run the interface
 if __name__ == "__main__":
